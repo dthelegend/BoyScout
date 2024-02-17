1093434969b935e58eb071f46da1a5511023f764
Adrian Vecina <avecinat@gmail.com>
2024-02-17 21:20:50 +0000

    ANGLES WORKING

--- boyscout/semaphore.py
+++ boyscout/semaphore.py
@@ -1,7 +1,8 @@
 import cv2
 import numpy as np
 from time import sleep as zzz
-from triangle import trianglesShareASide
+from triangle import trianglesShareASide, lineAngle
+import math
 
 # initialize the camera 
 # If you have multiple camera connected with  
@@ -81,6 +82,14 @@ def red(rgbimage):
 
     return contours
 
+def fixAngle(angle):
+
+    if angle > 360:
+        return angle - 360
+    if angle < 0:
+        return angle + 360
+    return angle
+
 def go():
 
     # reading the input using the camera 
@@ -101,7 +110,6 @@ def go():
         red_con_new = []
         for contour in red_con:
 
-            print(len(contour))
             # Approximate the contour
             epsilon = 0.04 * cv2.arcLength(contour, True)
             approx = cv2.approxPolyDP(contour, epsilon, True)
@@ -133,7 +141,8 @@ def go():
         for yt in yel_con_new:
             ypoints = np.vstack(yt).squeeze()
             for rt in red_con_new: 
-                rpoints = np.vstack(yt).squeeze()
+                rpoints = np.vstack(rt).squeeze()
+
                 if trianglesShareASide(ypoints, rpoints):
                     flags.append((yt, rt))
                     break 
@@ -141,10 +150,53 @@ def go():
         
 
         #Get all centroids
-        flagCen = [centroid(x) for x in flags]
+        flagCen = [(centroid(x[0]), centroid(x[1])) for x in flags]
 
-        for px,py in flagCen:
-            cv2.circle(redim, (px,py), 3, (0, 255, 0), -1)
+        flagCen.sort(key=lambda x: x[0])
+
+        if len(flagCen) == 2:
+            #LEFT
+            p1, p2 = flagCen[0]          
+            gradient = (p1[1]-p2[1])/(p1[0]-p2[0]+0.00001)
+            base = math.degrees(math.atan(gradient))
+            if p1[1] < p2[1]:  # YELLOW ABOVE
+                if gradient < 0:
+                    angle = 270 + base
+                else:
+                    angle = 90 + base
+            else: # YELLOW BELOW
+                if gradient < 0:
+                    angle = 90 + base
+                else:
+                    angle = 270 + base
+            angle -= 135
+            angle = fixAngle(angle)
+            cv2.putText(redim, f"L: {angle}d", (30, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255,0, 20), 2)
+
+            #RIGHT
+            p1, p2 = flagCen[1]    
+            gradient = (p2[1]-p1[1])/(p2[0]-p1[0]+0.00001)
+            base = math.degrees(math.atan(gradient))
+            if p1[1] < p2[1]:  # YELLOW ABOVE
+                if gradient < 0:
+                    angle = 270 + base
+                else:
+                    angle = 90 + base
+            else: # YELLOW BELOW
+                if gradient < 0:
+                    angle = 90 + base
+                else:
+                    angle = 270 + base
+            angle -= 225
+            angle = fixAngle(angle)
+            cv2.putText(redim, f"R: {angle}d", (30, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 255, 20), 2)
+
+
+            p1, p2 = flagCen[1]
+        for (p1,p2) in flagCen:
+            cv2.circle(redim, (p1), 3, (0, 255, 0), -1)
+            cv2.circle(redim, (p2), 3, (0, 255, 0), -1)
+            cv2.line(redim, p1, p2, (120, 50, 80), 2)
         
         cv2.imshow("conts", redim)
 
